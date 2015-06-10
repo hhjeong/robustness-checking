493b17bb72f7c371562c42cc37af5ae0941caeb8
admin <admin@localhost.localdomain>
2015-06-10 15:19:34 +0900

    added : shell script "experiment.sh" that runs MINA

---
+++ experiment.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+
+for((i=0;i<100;++i));
+do
+    ../MINA/bin/MINA -ip mRNA_filtered_random$i.txt -ip CNA_filtered_random$i.txt -ip METH_filtered_random$i.txt -o output/$i/ dist -s target_gene.txt -io clinical$i.txt;
+done
