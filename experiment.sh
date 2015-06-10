bfa8eaa038858ef89da7d7787595bf0457191fac
admin <admin@localhost.localdomain>
2015-06-10 15:43:05 +0900

    modified : experiment.sh. added MINA parameter -dhi 0.32

--- experiment.sh
+++ experiment.sh
@@ -2,5 +2,5 @@
 
 for((i=0;i<100;++i));
 do
-    ../MINA/bin/MINA -ip mRNA_filtered_random$i.txt -ip CNA_filtered_random$i.txt -ip METH_filtered_random$i.txt -o output/$i/ dist -s target_gene.txt -io clinical$i.txt;
+    ../../MINA/bin/MINA -ip mRNA_filtered_random$i.txt -ip CNA_filtered_random$i.txt -ip METH_filtered_random$i.txt -o output/$i/ dist -s target_gene.txt -io clinical$i.txt -dhi 0.3;
 done
