d3d4e152412f423bc6e80d4988881a7569b22878
hhjeong <libe@ajou.ac.kr>
2015-06-17 00:54:17 +0900

    first run

--- experiment.sh
+++ experiment.sh
@@ -1,6 +1,19 @@
 # !/bin/bash
 
-for((i=0;i<100;++i));
+EXE="../MINA/bin/MINA"
+SAM_PATH="sampling-data"
+RES_PATH="result"
+
+for((i=0;i<100;++i))
 do
-    ../../MINA/bin/MINA -ip mRNA_filtered_random$i.txt -ip CNA_filtered_random$i.txt -ip METH_filtered_random$i.txt -o output/$i/ dist -s target_gene.txt -io clinical$i.txt -dhi 0.3;
+    CMD="$EXE -ip ${SAM_PATH}/mRNA_filtered_random$i.txt";
+   	CMD+=" -ip ${SAM_PATH}/CNA_filtered_random$i.txt"
+   	CMD+=" -ip ${SAM_PATH}/METH_filtered_random$i.txt"
+   	CMD+=" -o ${RES_PATH}/"
+	CMD+=" dist"
+   	CMD+=" -s ${SAM_PATH}/target_gene.txt"
+   	CMD+=" -io ${SAM_PATH}/clinical$i.txt"
+	CMD+=" -dhi 0.3"
+	echo $CMD
+	$CMD;
 done
